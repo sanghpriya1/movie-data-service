82fae8d51578dbfcd7006d6c9723fd5667918548
Sanghparya  gautam <sanghparya@sanghparya.gautam>
2021-09-27 19:09:06 +0530

    1

---
+++ src/test/java/io/javabrains/moviedataservice/MovieDataServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package io.javabrains.moviedataservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MovieDataServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
